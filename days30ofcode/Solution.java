5f9c09a6a69953583c00494a288421e9c3f62859
Ilanna Freire <84036601+ilannafreire@users.noreply.github.com>
2025-02-06 15:31:54 -0300

    day0 config

---
+++ days30ofcode/Solution.java
@@ -0,0 +1,24 @@
+package days30ofcode;
+
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int intValue = scanner.nextInt();
+
+        double doubleValue = scanner.nextDouble();
+
+        scanner.nextLine();
+
+        String stringValue = scanner.nextLine();
+
+        scanner.close();
+
+        System.out.println("String: " + stringValue);
+        System.out.println("Double: " + doubleValue);
+        System.out.println("Int: " + intValue);
+    }
+}
